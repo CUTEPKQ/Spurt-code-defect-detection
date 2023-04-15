f07cf20bdce0e59b429b4012d64fe3c5682fa1b6
fyx <3231219729@qq.com>
2023-04-15 22:06:10 +0800

    生成图片

---
+++ src/Inkjet_code_data_generation/data_generation.py
@@ -0,0 +1,123 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+import random
+import csv
+import string
+
+
+
+# 英文字典
+# 数字，英文大小写
+en_dic = string.printable
+en_dic_list = list(en_dic)
+en_dic_list = en_dic_list[0:62]
+
+
+# 背景
+background_color = (255, 255, 255)
+
+
+current_path=os.path.dirname(__file__)
+# 加载字体文件
+font_path = os.path.join(current_path, 'ttf/point.ttf')
+# 保存位置
+save_folder =os.path.join(current_path,'../..','data/','origin_data/') 
+dicname_format = 'picture_{}/'
+picture_filename_format = 'picture_{}.jpg'
+csv_filename_format = 'picture_{}.csv'
+text_file = 'data/train_m.txt'
+
+
+def create_text():
+    """
+    # 个数数随机(1,17)
+     字符随机
+    :return: text
+    """
+    num = random.randint(1, 17)
+    text = ''
+    # 创建需要的字符串
+    for i in range(num):
+        random_char = random.choice(en_dic_list)
+        text += random_char
+    return text
+
+
+def create_data(picture_num):
+    for k in range(picture_num):
+
+        # 确认字体
+        font_size = random.randint(20, 40)
+        font = ImageFont.truetype(font_path, font_size)
+
+        # 确认文本
+        text = create_text()
+
+        # 初始文本框
+        bbox = font.getbbox(text)
+        # print('bbox' , bbox)
+        origin_w = bbox[2]
+        origin_h = bbox[3]
+        # print('origin', origin_w , origin_h)
+
+        # 创建图像
+        h = int((1 + random.random()) * origin_h)
+        w = int((1 + random.random()) * origin_w)
+        # print('img', w, h)
+        image_size = (w, h)
+
+        # 背景绘制
+        image = Image.new('RGB', image_size, background_color)
+        draw = ImageDraw.Draw(image)
+
+        # 文字位置 & 绘制
+        # output_size = (1, 1)
+        # draw.text(output_size, text, text_color, font=font)
+        text_color = (random.randint(20, 255), random.randint(20, 255), random.randint(20, 255))
+        origin_x = int(random.random() * (w - origin_w))
+        origin_y = int(random.random() * (h - origin_h))
+        output_size = (origin_x, origin_y)
+        draw.text(output_size, text, text_color, font=font)
+
+        # 确定保存路径
+        dicname = dicname_format.format(k + 1)
+        picture_filename = picture_filename_format.format(k + 1)
+        csv_filename = csv_filename_format.format(k + 1)
+        save_path = save_folder + dicname
+
+        # 起始
+        x = origin_x
+        # 保存text中每个char的位置信息
+        char_positions = []
+        for char in text:
+            # 字符宽高
+            char_width = font.getsize(char)[0]
+            char_height = font.getsize(char)[1]
+
+            char_bbox = (x, origin_y, char_height, char_width, char)
+            char_positions.append(char_bbox)
+            x += char_width
+
+        print(text, char_positions)
+
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        for bbox in char_positions:
+            draw.rectangle((bbox[0], bbox[1], bbox[0] + bbox[3], bbox[1] + bbox[2]), outline="black")
+
+        # bbox = font.getbbox(text)
+        # print('bbox' , bbox)
+        # draw.rectangle(bbox, outline="black")
+
+        image.save(save_path + picture_filename, 'JPEG')
+
+        with open(save_path + csv_filename, 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(['x', 'y', 'h', 'w', 'info'])
+            for bbox in char_positions:
+                x, y, h, w, info = bbox
+                writer.writerow([x, y, h, w, info])
+
+
+create_data(5)
--- src/data_process/defect_generation.py
+++ src/data_process/defect_generation.py
@@ -1,85 +1,70 @@
 '''
 Author: fyx
 Date: 2023-04-10 18:22:37
-LastEditTime: 2023-04-10 21:06:05
+LastEditTime: 2023-04-14 15:59:56
 Description: 缺陷生成
 '''
 import cv2
 import numpy as np
+import os
+import pandas as pd
+
+
 
 class DataProcessor:
-    def __init__(self,path:str= None) -> None:
+    def __init__(self,path:str= None,num:int= None) -> None:
         # 加载原始喷码图像
-        self.img=cv2.imread(path)
-        self.radius=1
+        self.path=path
+        self.num=num
 
-    def draw_circle(self,event, x, y,flags,param)-> None:
+    def data_process(self,img_path:str= None,info_path:str= None)-> None:
+        '''
+        Author: fyx
+        description: 对图片进行缺陷生成
+        '''
+        for id in range(self.num):
+            #得到图片和信息路径
+            path_name='picture_'+str(id)
+            flie_path=os.path.join(self.path,path_name)
+            picture_name=path_name+'.jpg'
+            info_name=path_name+'.csv'
+            
 
-        if event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
-            cv2.circle(self.img, (x, y), self.radius, (0,0,0), -1)
- 
-        elif event == cv2.EVENT_LBUTTONUP:
-            cv2.circle(self.img, (x, y), self.radius, (0,0,0), -1)
- 
+            
 
-    def draw_rectangle(self,event, x, y,flags,param) -> None:
-        if event == cv2.EVENT_LBUTTONDOWN:
-             # 记录矩形的起始点坐标
-            param["start_point"] = (x, y)
-        elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
-            #cv2.rectangle(self.img, param["start_point"], (x, y), (0, 0, 255), 2)
-            cv2.rectangle(self.img, param["start_point"], (x, y), (255, 255, 255), -1)
-        elif event == cv2.EVENT_LBUTTONUP:
-            # 记录矩形的结束点坐标
-            param["end_point"] = (x, y)
-            # x1, y1 = param["start_point"]
-            # x2, y2 = param["end_point"]
-            # x, y = min(x1, x2), min(y1, y2)
-            # w, h = abs(x2 - x1), abs(y2 - y1)
-            cv2.rectangle(self.img, param["start_point"], param["end_point"], (255, 255, 255), -1)
+
+
+
         
-    def img_show(self, img_result:np= None)-> None:
-        cv2.imshow('result', img_result)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    
 
-    def img_loss(self)-> None:
-        # 显示原始图像，并设置鼠标回调函数
-        cv2.namedWindow('image')
-        cv2.setMouseCallback('image', self.draw_rectangle,{"start_point": None, "end_point": None})
-        # 循环显示图像，直到按下ESC键退出
-        while True:
-            cv2.imshow("image", self.img)
-            if cv2.waitKey(1) & 0xFF == 27:
-                cv2.imwrite("result.jpg", self.img)
-                break
+    def img_loss(self,picture_path:str= None,info_path:str= None)-> None:
+        '''
+        description: 
+        ''' 
+
+        
+      
+        
+          
+        pass
+
     
     def img_add(self)-> None:
-        # 显示原始图像，并设置鼠标回调函数
-        cv2.namedWindow('image')
-        cv2.setMouseCallback('image', self.draw_circle)
-        while True:
-            cv2.imshow('image', self.img)
-            if cv2.waitKey(1) & 0xFF == 27:
-                cv2.imwrite("result.jpg", self.img)
-                break
-        # 将多余的喷墨应用于原始图像
+        pass
+
   
     
-    def img_shift(self,dx:int= None,dy:int= None) ->None:
-        # 定义仿射变换矩阵
-        M = np.float32([[1, 0, dx], [0, 1, dy]])
-        # 应用仿射变换
-        result = cv2.warpAffine(self.img, M, (self.img.shape[1], self.img.shape[0]))
-
-        self.img_show(result)
-        
+    def img_shift(self) ->None:
+        pass
 
 
 def main()-> None:
-    path="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/2.jpg"
-    processor=DataProcessor(path)
-    processor.img_shift(40,60)
+    path="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/1.png"
+    img=cv2.imread(path)
+    print(img)
+    # processor=DataProcessor(path)
+    # processor.img_shift(20,30)
 
 
 if __name__ == '__main__':
