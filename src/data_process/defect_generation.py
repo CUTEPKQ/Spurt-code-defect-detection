d5523478c8bbf7ce76a010f978d3b9c39727fa58
fyx <3231219729@qq.com>
2023-04-17 17:52:01 +0800

    fix bugs

--- src/data_process/defect_generation.py
+++ src/data_process/defect_generation.py
@@ -1,7 +1,7 @@
 '''
 Author: fyx
 Date: 2023-04-10 18:22:37
-LastEditTime: 2023-04-17 09:02:47
+LastEditTime: 2023-04-17 17:45:18
 Description: 缺陷生成
 '''
 import cv2
@@ -30,7 +30,7 @@ class DataProcessor:
             flie_path=os.path.join(self.path,path_name)
             picture_name=path_name+'.jpg'
             info_name=path_name+'.csv'
-
+            
             
 
 
@@ -39,30 +39,30 @@ class DataProcessor:
         description: 处理缺陷
         ''' 
         img=cv2.imread(picture_path)
+        print(img.shape)
         img_pd=pd.read_csv(info_path)
-        select=random.randint(0,img_pd.shape[0])
+        select=random.randint(0,img_pd.shape[0]-1)
         sub_pd=img_pd.iloc[select]
         location=sub_pd.values[:-1]
         result=img.copy()
-        save_img_fold=os.path.join(self.save_path,'processed_picture'+str(id))
-        if os.path.exists(save_img_fold):
+        save_img_fold=os.path.join(self.save_path,'processed_data','picture'+str(id))
+        if not os.path.exists(save_img_fold):
             os.makedirs(save_img_fold)
         mode=1
         #mode=1,单个字符漏印
         if mode==1:
-            mask=np.zeros((tuple(location[2:])),np.uint8)
-            result[location[0]:location[0]+location[2],location[1]:location[1]+location[3],:]=mask
-            save_img_path=os.path.join(save_img_fold,'loss1_picture'+str(id))
+            result[location[0]:location[0]+location[2],location[1]:location[1]+location[3],:]=[255,255,255]
+            save_img_path=os.path.join(save_img_fold,'loss1_picture'+str(id)+'.jpg')
             cv2.imwrite(save_img_path,result)
             print(f'successfully get loss1 form picture{id}')
         ## mode==2 缺失
         elif mode==2:
             h=random.randint(0,result.shape[0])
             start=random.randint(0,result.shape[0]-h)
-            result[start:start+h, :,]=[0,0,0]
-            save_img_path=os.path.join(save_img_fold,'loss1_picture'+str(id))
+            result[start:start+h, :,]=[255,255,255]
+            save_img_path=os.path.join(save_img_fold,'loss2_picture'+str(id)+'.jpg')
             cv2.imwrite(save_img_path,result)
-            print(f'successfully get loss1 form picture{id}')   
+            print(f'successfully get loss2 form picture{id}')   
           
     def img_add(self,picture_path:str= None,info_path:str= None,id:int= None)-> None:
         img=cv2.imread(picture_path)
@@ -97,10 +97,11 @@ class DataProcessor:
 
 
 def main()-> None:
-    path="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/1.png"
-    img=cv2.imread(path)
-    print(img)
-    # processor=DataProcessor(path)
+    path3='D:/Desktop/Destop_files/Spurt-code-defect-detection/data/'
+    path1="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/origin_data/picture_1/picture_1.jpg"
+    path2="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/origin_data/picture_1/picture_1.csv"
+    processor=DataProcessor(path1,2,path3)
+    processor.img_loss(path1,path2,1)
     # processor.img_shift(20,30)
 
 
