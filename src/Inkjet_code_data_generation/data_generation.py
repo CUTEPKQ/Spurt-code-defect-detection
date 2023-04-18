5e75c7105bfe6be6424a453b34edfe361c02cf8e
fyx <3231219729@qq.com>
2023-04-18 20:31:23 +0800

    生成缺陷

--- src/Inkjet_code_data_generation/data_generation.py
+++ src/Inkjet_code_data_generation/data_generation.py
@@ -4,18 +4,17 @@ import random
 import csv
 import string
 
-
-
 # 英文字典
 # 数字，英文大小写
 en_dic = string.printable
+# print(en_dic)
+
 en_dic_list = list(en_dic)
 en_dic_list = en_dic_list[0:62]
 
 
 # 背景
 background_color = (255, 255, 255)
-
 
 current_path=os.path.dirname(__file__)
 # 加载字体文件
@@ -36,22 +35,119 @@ def create_text():
     """
     num = random.randint(1, 17)
     text = ''
+    text_idx = []
     # 创建需要的字符串
     for i in range(num):
         random_char = random.choice(en_dic_list)
         text += random_char
-    return text
+        text_idx.append(i)
+    return text,text_idx
+
+
+
+
+def get_pos_rate_dic(text,font_size):
+    # text = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    font = ImageFont.truetype(font_path, font_size)
+
+    # 初始文本框
+    bbox = font.getbbox(text)
+
+    origin_w = bbox[2]
+    origin_h = bbox[3]
+
+    image_size = (origin_w, origin_h)
+    image = Image.new('RGB', image_size, background_color)
+    draw = ImageDraw.Draw(image)
+
+    image = Image.new('RGB', image_size, background_color)
+    # 背景
+    draw = ImageDraw.Draw(image)
+
+    # word
+    draw.text([0,0], text, 0 , font=font)
+
+    x = 0
+    origin_y =0
+    # 保存text中每个char的位置信息
+    char_positions = []
+    for char in text:
+        # 字符宽高
+        char_width = font.getsize(char)[0]
+        char_height = font.getsize(char)[1]
+
+        char_bbox = (x, origin_y, char_height, char_width, char)
+        char_positions.append(char_bbox)
+        x += char_width
+
+    # 标准位置
+    # for bbox in char_positions:
+    #     draw.rectangle((bbox[0], bbox[1], bbox[0] + bbox[3], bbox[1] + bbox[2]), outline="black")
+
+
+    # 创建对应比例的映射
+    h_rate_list = []
+    w_rate_list = []
+
+
+    for bbox in char_positions:
+        w_rate=0
+        # 从右往左，get  w
+        flag = 0
+        for i in range( bbox[0]-1 + bbox[3] , bbox[0] , -1):
+            for j in range( bbox[1]-1 ,bbox[1] + bbox[2]-1):
+                if image.getpixel((i,j)) !=(255,255,255):
+                    # print('i: ',i, 'bbox[0]:' , bbox[0], '3: ' ,bbox[3],'res ;',w_rate)
+                    w_rate =(i-bbox[0]) / bbox[3]
+                    w_rate_list.append(w_rate)
+                    flag = 1
+                    break
+            if flag:
+                break
+
+
+
+    for bbox in char_positions:
+        h_rate = 0
+        flag = 0
+        for j in range(bbox[1]+1, bbox[1] + bbox[2]):
+            for i in range( bbox[0] + bbox[3] -1, bbox[0] , -1):
+                if image.getpixel((i,j)) !=(255,255,255):
+                    h_rate =(bbox[1] + bbox[2]-j) / bbox[2]
+                    h_rate_list.append(h_rate)
+                    flag = 1
+                    break
+            if flag:
+                break
+
+
+
+    # print((h_rate_list))
+    # image.show()
+    return h_rate_list,w_rate_list
+
+
+
 
 
 def create_data(picture_num):
+
+
+
     for k in range(picture_num):
 
         # 确认字体
-        font_size = random.randint(20, 40)
+        font_size = random.randint(20, 70)
         font = ImageFont.truetype(font_path, font_size)
 
-        # 确认文本
-        text = create_text()
+        # 确认文本,和序列
+        text,text_idx = create_text()
+        h_rate_list, w_rate_list = get_pos_rate_dic(text, 40)
+
+        # print(font_size)
+        # text = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        # text_idx = [i for i in range(0,62)]
+        # print(text_idx)
 
         # 初始文本框
         bbox = font.getbbox(text)
@@ -85,23 +181,42 @@ def create_data(picture_num):
         csv_filename = csv_filename_format.format(k + 1)
         save_path = save_folder + dicname
 
+
         # 起始
         x = origin_x
         # 保存text中每个char的位置信息
         char_positions = []
-        for char in text:
+
+
+        for char,idx in zip(text,text_idx):
             # 字符宽高
             char_width = font.getsize(char)[0]
             char_height = font.getsize(char)[1]
 
-            char_bbox = (x, origin_y, char_height, char_width, char)
+            # new y
+            y = origin_y
+            y = int((y+char_height) - char_height * h_rate_list[idx]) -1
+
+            # new height
+            h = int(char_height * h_rate_list[idx]) + 1
+
+            # new width
+            w = int(char_width * w_rate_list[idx]) + 1
+            # print(char,x)
+
+            char_bbox = (x, y, h, w, char)
             char_positions.append(char_bbox)
+
             x += char_width
 
-        print(text, char_positions)
+        # print(text, char_positions)
+
+
 
         if not os.path.exists(save_path):
             os.makedirs(save_path)
+
+
 
         for bbox in char_positions:
             draw.rectangle((bbox[0], bbox[1], bbox[0] + bbox[3], bbox[1] + bbox[2]), outline="black")
@@ -120,4 +235,7 @@ def create_data(picture_num):
                 writer.writerow([x, y, h, w, info])
 
 
-create_data(5)
+create_data(10)
+
+
+# get_pos_rate_dic(20)
--- src/data_process/defect_generation.py
+++ src/data_process/defect_generation.py
@@ -1,7 +1,7 @@
 '''
 Author: fyx
 Date: 2023-04-10 18:22:37
-LastEditTime: 2023-04-17 17:45:18
+LastEditTime: 2023-04-18 19:44:11
 Description: 缺陷生成
 '''
 import cv2
@@ -9,6 +9,11 @@ import numpy as np
 import os
 import pandas as pd
 import random
+
+def random_odd_number(a, b) -> int:
+    num = np.random.randint(a, b+1)
+    num = num if num % 2 == 1 else num + 1  # 如果生成的数为偶数，则加1变成奇数
+    return num
 
 
 class DataProcessor:
@@ -19,18 +24,23 @@ class DataProcessor:
         self.save_path=save_path
         
 
-    def data_process(self,img_path:str= None,info_path:str= None)-> None:
+    def data_process(self)-> None:
         '''
         Author: fyx
         description: 对图片进行缺陷生成
         '''
-        for id in range(self.num):
+        for id in range(1,self.num+1):
             #得到图片和信息路径
             path_name='picture_'+str(id)
-            flie_path=os.path.join(self.path,path_name)
-            picture_name=path_name+'.jpg'
-            info_name=path_name+'.csv'
-            
+            file_path=os.path.join(self.path,path_name)
+            picture_name=os.path.join(file_path,path_name+'.jpg')
+            print(picture_name)
+            info_name=os.path.join(file_path,path_name+'.csv')
+            self.img_loss(picture_path=picture_name,info_path=info_name,id=id)
+            self.img_blur(picture_path=picture_name,id=id)
+            self.img_add(picture_path=picture_name,id=id)
+        
+        print("successfully process pictures")
             
 
 
@@ -39,69 +49,87 @@ class DataProcessor:
         description: 处理缺陷
         ''' 
         img=cv2.imread(picture_path)
-        print(img.shape)
         img_pd=pd.read_csv(info_path)
-        select=random.randint(0,img_pd.shape[0]-1)
-        sub_pd=img_pd.iloc[select]
-        location=sub_pd.values[:-1]
         result=img.copy()
-        save_img_fold=os.path.join(self.save_path,'processed_data','picture'+str(id))
+        save_img_fold=os.path.join(self.save_path,'picture'+str(id))
         if not os.path.exists(save_img_fold):
             os.makedirs(save_img_fold)
-        mode=1
+        mode=np.random.randint(1,2)
         #mode=1,单个字符漏印
         if mode==1:
-            result[location[0]:location[0]+location[2],location[1]:location[1]+location[3],:]=[255,255,255]
+            select=random.randint(0,img_pd.shape[0]-1)
+            sub_pd=img_pd.iloc[select]
+            print('缺失字符为',end='')
+            print(sub_pd['info'])
+            location=sub_pd.values[:-1]
+            result[location[1]:location[1]+location[2],location[0]:location[0]+location[3],:]=[255,255,255]
             save_img_path=os.path.join(save_img_fold,'loss1_picture'+str(id)+'.jpg')
             cv2.imwrite(save_img_path,result)
             print(f'successfully get loss1 form picture{id}')
         ## mode==2 缺失
         elif mode==2:
-            h=random.randint(0,result.shape[0])
+            h=random.randint(10,result.shape[0])
             start=random.randint(0,result.shape[0]-h)
-            result[start:start+h, :,]=[255,255,255]
+            result[start:start+h]=[255,255,255]
             save_img_path=os.path.join(save_img_fold,'loss2_picture'+str(id)+'.jpg')
             cv2.imwrite(save_img_path,result)
             print(f'successfully get loss2 form picture{id}')   
           
-    def img_add(self,picture_path:str= None,info_path:str= None,id:int= None)-> None:
-        img=cv2.imread(picture_path)
-        img_info=pd.read_csv(info_path)
-        # 随机生成污染区域的位置和大小
-        h, w = img.shape[:2]
-        x = np.random.randint(0, w-100)
-        y = np.random.randint(0, h-100)
-        width = np.random.randint(50, 100)
-        height = np.random.randint(50, 100)
+    def img_add(self,picture_path:str= None,id:int= None)-> None:
+        save_img_fold=os.path.join(self.save_path,'picture'+str(id))
+        if not os.path.exists(save_img_fold):
+            os.makedirs(save_img_fold)
 
-        # 随机生成污染区域的形状
-        num_points = np.random.randint(5, 20)
-        points = np.zeros((num_points, 2), dtype=np.int32)
-        for i in range(num_points):
-            points[i, 0] = np.random.randint(x, x+width)
-            points[i, 1] = np.random.randint(y, y+height)
-
-        # 创建掩模图像
-        mask = np.zeros_like(img)
-        cv2.fillPoly(mask, [points], (255, 255, 255))
-
-        # 将掩模应用于原图像
-        result = cv2.bitwise_and(img, mask)
-
-        pass
-
+        # 读取图片
+        img = cv2.imread(picture_path)
+        # 创建一个空白掩膜，尺寸与原始图像相同
+        mask = np.zeros(img.shape[:2], dtype=np.uint8)
+        # 随机生成一个圆形区域
+        radius=np.random.randint(10,20)
+        center=(np.random.randint(radius,img.shape[1]-radius),
+                np.random.randint(radius,img.shape[0]-radius))
+        cv2.circle(mask, center, radius, 255, -1)
+        # 随机生成内核的形状、大小和方向
+        kernel_shape = np.random.choice([cv2.MORPH_RECT, cv2.MORPH_CROSS, cv2.MORPH_ELLIPSE])
+        kernel_size = np.random.randint(3, 10)
+        kernel_rotation = np.random.randint(0, 180)
+        # 创建随机形状的内核
+        kernel = cv2.getStructuringElement(kernel_shape, (kernel_size, kernel_size))
+        kernel = cv2.warpAffine(kernel, cv2.getRotationMatrix2D((kernel_size//2, kernel_size//2), 
+        kernel_rotation, 1.0), (kernel_size, kernel_size))
+        # 对原始图像进行膨胀操作，重复多次以增加目标物体的面积
+        iterations = 3
+        mask = cv2.dilate(mask, kernel, iterations=iterations)
+        # 将掩膜反转，变成黑色圆形区域的掩膜
+        mask = cv2.bitwise_not(mask)
+        # 将原始图像和掩膜进行与操作，保留圆形区域以外的颜色
+        result = cv2.bitwise_and(img, img, mask=mask)
+        save_img_path=os.path.join(save_img_fold,'loss2_picture'+str(id)+'.jpg')
+        cv2.imwrite(save_img_path,result)
+        print(f'successfully  pollute  picture{id}') 
   
-    
-    def img_shift(self) ->None:
-        pass
+    def img_blur(self,picture_path:str= None,id:int= None)-> None:
+        save_img_fold=os.path.join(self.save_path,'picture'+str(id))
+        if not os.path.exists(save_img_fold):
+            os.makedirs(save_img_fold)
+        # 读取图片
+        img = cv2.imread(picture_path)
+        noise = np.zeros(img.shape, np.uint8)
+        cv2.randn(noise, 0, 50)
+        noisy_img = cv2.add(img, noise)
+        kernel_size=np.random.choice([i for i in range(7,20,2)])
+        # 进行高斯滤波
+        result= cv2.GaussianBlur(noisy_img, (kernel_size,kernel_size),5)
+        save_img_path=os.path.join(save_img_fold,'loss2_picture'+str(id)+'.jpg')
+        cv2.imwrite(save_img_path,result)
+        print(f'successfully  blur  picture{id}') 
 
 
 def main()-> None:
-    path3='D:/Desktop/Destop_files/Spurt-code-defect-detection/data/'
-    path1="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/origin_data/picture_1/picture_1.jpg"
-    path2="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/origin_data/picture_1/picture_1.csv"
-    processor=DataProcessor(path1,2,path3)
-    processor.img_loss(path1,path2,1)
+    picture_path="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/origin_data/"
+    save_path="D:/Desktop/Destop_files/Spurt-code-defect-detection/data/process_data/"
+    processor=DataProcessor(picture_path,10,save_path)
+    processor.data_process()
     # processor.img_shift(20,30)
 
 
